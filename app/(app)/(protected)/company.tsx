50bb3133edfd93416eda7fba5601b0d3da56132f
Stanisław Marciniak <marciniak.dev@gmail.com>
2024-10-26 00:02:59 +0200

    setup

--- app/(app)/(protected)/company.tsx
+++ app/(app)/(protected)/company.tsx
@@ -10,18 +10,7 @@ export default function Settings() {
 
 	return (
 		<View className="flex-1 p-4 bg-background gap-y-4">
-			<H1 className="">Hi Alice</H1>
-			<Muted className="text-center">
-				Sign out and return to the welcome screen.
-			</Muted>
-			<Button
-				className="w-full"
-				size="default"
-				variant="default"
-				onPress={signOut}
-			>
-				<Text>Sign Out</Text>
-			</Button>
+			<H1 className="mb-8">Hi Alice</H1>
 		</View>
 	);
 }
