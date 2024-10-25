fe7e9e22b8c9300a88d8e8b3e95c51726f51ce6d
bnowacki <bknowacki@gmail.com>
2024-10-25 23:41:58 +0200

    gluestack

---
+++ app/(app)/(protected)/leaderboard.tsx
@@ -0,0 +1,27 @@
+import { View } from "react-native";
+
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { H1, Muted } from "@/components/ui/typography";
+import { useSupabase } from "@/context/supabase-provider";
+
+export default function Settings() {
+	const { signOut } = useSupabase();
+
+	return (
+		<View className="flex-1 p-4 bg-background gap-y-4">
+			<H1 className="">Leaderboard</H1>
+			<Muted className="text-center">
+				Sign out and return to the welcome screen.
+			</Muted>
+			<Button
+				className="w-full"
+				size="default"
+				variant="default"
+				onPress={signOut}
+			>
+				<Text>Sign Out</Text>
+			</Button>
+		</View>
+	);
+}
